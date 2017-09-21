fafb7aa9a829c5f0379c34e7fe34af76b2901de9
mitsupro <31908890+mitsupro@users.noreply.github.com>
2017-09-21 19:45:56 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,4 +1,6 @@
 <?php
+$proxy = 'tricycle';
+$proxyauth = '	http://fixie:lGL3ZqnFSaYC8Rw@velodrome.usefixie.com:80';
 $access_token = 'JDxyQwsiEXhl1GJL0dk1qESO+pF4XNpNidsKqdUH4f68q/DgEG+3geq0Rie2VilnRV20bJmpVW9LyeJNbgZy25/G1KdNy26goS0LxhEGlD7YE+0znHvJVDfvPLjYQMS1DwNXXeQXrXyY3cW4KNV8xwdB04t89/1O/w1cDnyilFU=';
 
 $url = 'https://api.line.me/v1/oauth/verify';
