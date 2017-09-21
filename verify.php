35793e6930539d15e80cfce2edfec6379c67807c
mitsupro <31908890+mitsupro@users.noreply.github.com>
2017-09-21 17:28:45 +0700

    Create verify.php

---
+++ verify.php
@@ -0,0 +1,15 @@
+<?php
+$access_token = 'JDxyQwsiEXhl1GJL0dk1qESO+pF4XNpNidsKqdUH4f68q/DgEG+3geq0Rie2VilnRV20bJmpVW9LyeJNbgZy25/G1KdNy26goS0LxhEGlD7YE+0znHvJVDfvPLjYQMS1DwNXXeQXrXyY3cW4KNV8xwdB04t89/1O/w1cDnyilFU=';
+
+$url = 'https://api.line.me/v1/oauth/verify';
+
+$headers = array('Authorization: Bearer ' . $access_token);
+
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+$result = curl_exec($ch);
+curl_close($ch);
+
+echo $result;
